8d75e577f5f15deec0c2068521a4a62768af5e13
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-28 14:10:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function firstWord(s) {
-  // your code here
-}
+function firstWord(string) {
+	
+  let num = string.trim().split(' ')
+
+  return num[0]
+} 
 
 // Do not change the code below
-
+  
 const s = prompt("Enter String:");
 alert(firstWord(s));
